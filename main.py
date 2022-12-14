5c2cd322b187710e88e33bf08d4df447b2659e20
svm5 <sveta6328s@yandex.ru>
2022-12-14 17:22:12 +0300

    Добавлен класс StartPage и переход на test_room

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import sys
 
 import pygame
 
+import test_room
+
 
 WIDTH = 1000
 HEIGHT = 800
-LIGTH_BLUE = (0, 0, 150)
+DARK_BLUE = (0, 0, 150)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 START_PAGE = 1
+
+cur_page = START_PAGE
 
 
 class Button:
@@ -24,13 +28,22 @@ class Button:
 
     def set_color(self, state):
         if state:
-            self.color = LIGTH_BLUE
+            self.color = DARK_BLUE
         else:
             self.color = BLUE
+
+    def set_func(self, func):
+        self.func = func
 
     def check_mouse_position(self, mouse_pos):
         return self.pos_x <= mouse_pos[0] < self.pos_x + self.width and \
                 self.pos_y <= mouse_pos[1] < self.pos_y + self.heigth
+    
+    def action(self):
+        try:
+            self.func()
+        except:
+            print("Пока у кнопки нет функции нет")
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.pos_x, self.pos_y, self.width, self.heigth))
@@ -40,62 +53,79 @@ class Button:
         screen.blit(text, text_rect)
         # screen.blit(text, (self.pos_x + self.width // 2, self.pos_y + self.heigth // 2))
 
-def draw_start_page(screen, mouse_pos):
-    all_sprites = pygame.sprite.Group()
-    background = pygame.sprite.Sprite()
-    background.image = pygame.image.load("res/backgrounds/volcanoes.jpg").convert()
-    background.image = pygame.transform.scale(background.image, (WIDTH, HEIGHT))
-    background.rect = background.image.get_rect()
-    all_sprites.add(background)
-    all_sprites.draw(screen)
+class StartPage:
+    def __init__(self):
+        self.all_sprites = pygame.sprite.Group()
+        self.prepare_sprites()
+        
+        self.buttons = []
+        self.prepare_buttons()
 
-    buttons = []
-    button_width = WIDTH // 3
-    button_height = HEIGHT // 12
-    font_size = HEIGHT // 20
-    step = HEIGHT // 30
+    def prepare_sprites(self):
+        self.background = pygame.sprite.Sprite()
+        self.background.image = pygame.image.load("res/backgrounds/volcanoes.jpg").convert()
+        self.background.image = pygame.transform.scale(self.background.image, (WIDTH, HEIGHT))
+        self.background.rect = self.background.image.get_rect()
+        self.all_sprites.add(self.background)
 
-    # кнопки
-    # начало игры
-    button_begin_game = Button(WIDTH // 3, HEIGHT // 2, button_width, button_height, BLUE, "НАЧАТЬ ИГРУ", font_size, WHITE)
-    button_begin_game.set_color(button_begin_game.check_mouse_position(mouse_pos))
-    buttons.append(button_begin_game)
+    def prepare_buttons(self):
+        button_width = WIDTH // 3
+        button_height = HEIGHT // 12
+        font_size = HEIGHT // 20
+        step = HEIGHT // 30
 
-    # магазины
-    button_shops = Button(WIDTH // 3, HEIGHT // 2 + 1 * step + 1 * button_height, button_width, button_height, BLUE, "МАГАЗИНЫ", font_size, WHITE)
-    button_shops.set_color(button_shops.check_mouse_position(mouse_pos))
-    buttons.append(button_shops)
+        # начало игры
+        self.button_begin_game = Button(WIDTH // 3, HEIGHT // 2, button_width, button_height, BLUE, "НАЧАТЬ ИГРУ", font_size, WHITE)
+        self.button_begin_game.set_func(test_room.main)
+        self.buttons.append(self.button_begin_game)
 
-    # настройки
-    button_settings = Button(WIDTH // 3, HEIGHT // 2 + 2 * step + 2 * button_height, button_width, button_height, BLUE, "НАСТРОЙКИ", font_size, WHITE)
-    button_settings.set_color(button_settings.check_mouse_position(mouse_pos))
-    buttons.append(button_settings)
+        # магазины
+        self.button_shops = Button(WIDTH // 3, HEIGHT // 2 + 1 * step + 1 * button_height, button_width, button_height, BLUE, "МАГАЗИНЫ", font_size, WHITE)
+        self.buttons.append(self.button_shops)
 
-    # правила игры
-    button_rules = Button(WIDTH // 3, HEIGHT // 2 + 3 * step + 3 * button_height, button_width, button_height, BLUE, "ПРАВИЛА ИГРЫ", font_size, WHITE)
-    button_rules.set_color(button_rules.check_mouse_position(mouse_pos))
-    buttons.append(button_rules)
+        # настройки
+        self.button_settings = Button(WIDTH // 3, HEIGHT // 2 + 2 * step + 2 * button_height, button_width, button_height, BLUE, "НАСТРОЙКИ", font_size, WHITE)
+        self.buttons.append(self.button_settings)
 
-    for button in buttons:
-        button.draw(screen)
+        # правила игры
+        self.button_rules = Button(WIDTH // 3, HEIGHT // 2 + 3 * step + 3 * button_height, button_width, button_height, BLUE, "ПРАВИЛА ИГРЫ", font_size, WHITE)
+        self.buttons.append(self.button_rules)
+
+    def check_transition(self, mouse_pos):
+        for button in self.buttons:
+            if button.check_mouse_position(mouse_pos) is True:
+                button.action()
+    
+    def draw_start_page(self, screen, mouse_pos):
+        self.all_sprites.draw(screen)
+
+        self.button_begin_game.set_color(self.button_begin_game.check_mouse_position(mouse_pos))
+        self.button_shops.set_color(self.button_shops.check_mouse_position(mouse_pos))
+        self.button_settings.set_color(self.button_settings.check_mouse_position(mouse_pos))
+        self.button_rules.set_color(self.button_rules.check_mouse_position(mouse_pos))
+
+        for button in self.buttons:
+            button.draw(screen)
 
 
 def main():
     pygame.init()
     pygame.display.set_caption("Малелькая колдунья")
-    cur_page = START_PAGE
 
     size = WIDTH, HEIGHT
     screen = pygame.display.set_mode(size)
+    page = StartPage()
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                page.check_transition(event.pos)
         mouse = pygame.mouse.get_pos()
-        if cur_page == START_PAGE :
-            draw_start_page(screen, mouse)
+        if cur_page == START_PAGE:
+            page.draw_start_page(screen, mouse)
         pygame.display.flip()
     pygame.quit()
 
